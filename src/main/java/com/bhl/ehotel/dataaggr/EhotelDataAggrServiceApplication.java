03e67f44edfa40a105fa8ce14748021cacc2fd47
WeatherBaren <baohailong@sina.com>
2018-10-30 09:13:45 +0800

    first commit

---
+++ src/main/java/com/bhl/ehotel/dataaggr/EhotelDataAggrServiceApplication.java
@@ -0,0 +1,31 @@
+package com.bhl.ehotel.dataaggr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class EhotelDataAggrServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhotelDataAggrServiceApplication.class, args); 
+	}
+	
+	@Bean
+	public JedisPool jedisPool() {
+		JedisPoolConfig config = new JedisPoolConfig();
+		config.setMaxTotal(100);
+		config.setMaxIdle(5);
+		config.setMaxWaitMillis(1000 * 10); 
+		config.setTestOnBorrow(true);
+		return new JedisPool(config, "10.0.0.3", 1111);
+	}
+	
+}
